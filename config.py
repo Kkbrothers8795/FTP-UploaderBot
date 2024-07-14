1c175b72b08e3e69e2a214239cbb5ec765ef3e4e
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-07-14 15:32:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
 
 #API ID & API Hash -----> my.telegram.org
-api_id = 924859
-api_hash = 'a4c9a18cf4d8cb24062ff6916597f832'
+api_id = 2532603
+api_hash = 'f565b00bbe3ad9c6748e39a3a71d16e7'
 
 #Bot Token -----> @BotFather
-token = '6898058070:AAERmk33JiLIdk1HCEI0-BUDJU2nY9EANx8'
+token = '7132343147:AAGHv_wmRd_7PRXeLsa9r42Zl28EbyR5V0E'
 
 #Session Name -----> optional
 session_name = 'FTP_Uploader'
 
 
 #The robot admin (the person who can give orders to the robot.) -----> @myidbot
-admins = [6113077431]
+admins = [754495556]
 
 # Chat id to send technical logs
-dev = 6113077431
+dev = 754495556
 
 # When a file is sent to the bot, first that file is downloaded from the Telegram repository and stored in the bot's server.
 # Here you need to specify its temporary storage path
@@ -24,11 +24,11 @@ dl_path = os.path.abspath(os.getcwd()) + '/'
 
 
 # The upload path where we give FTP access to the bot.
-ftp_path = '/hxbots.rf.gd/htdocs/'
+ftp_path = '/earn.22web.org/htdocs/FTPXBot/'
 
 # The files are temporarily downloaded after they are on the bot server. They are uploaded to another host through FTP.
 # Here we have to give FTP access to the bot.
 ftp_ip = 'ftpupload.net'
-ftp_username = 'if0_35501023'
-ftp_password = 'ilIAIplvVwZU'
-ftp_domain = 'http://hxbots.rf.gd/'
+ftp_username = 'rigrr_34652687'
+ftp_password = 'nrq0pgqv'
+ftp_domain = 'http://earn.22web.org/FTPXBot/'
